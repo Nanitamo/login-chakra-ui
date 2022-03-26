0d531d4e034c29f863720db37ba5d39c870a6f08
Nanitamo <nanitamo19@gmail.com>
2022-03-26 01:51:35 +0100

    remove function onLoad

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   const formBackground = useColorModeValue("gray.100", "gray.700")
   
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center" onLoad={toggleColorMode}>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
       <Flex direction="column" background={formBackground} p={12} rounded={6}>
         <Heading mb={6}>Login</Heading>
         <Input placeholder="nanitamo19@gmail.com" variant="filled" mb={3} type="email" />
